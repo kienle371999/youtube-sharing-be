8deedf3bcde88cc7594dab4c55df9abad17e0eff
kienle371999 <kienle371999@gmail.com>
2024-04-07 10:22:14 +0700

    [YS-BE] Update swagger

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,45 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
-import * as compression from 'compression';
-import * as cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { ENV } from './config/environment';
 import initAPIDocs from './config/swagger';
 import { AppErrorHandler } from './middleware/app-error-handler';
+
+const globalPrefix = 'api';
+const swaggerEndpoint = 'api-docs';
 
 async function bootstrap() {
   const logger = new Logger('main');
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix(`api`);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-
-  const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AppErrorHandler(httpAdapter));
-
-  app.use(cookieParser());
-  app.use(helmet());
-  app.use(compression());
+  app.useGlobalFilters(
+    new AppErrorHandler(app.get(HttpAdapterHost).httpAdapter),
+  );
 
   app.enableCors({
     origin: '*',
     methods: '*',
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization',
+    allowedHeaders: 'Content-Type, Accept, Authorization, Cache-Control',
   });
 
   initAPIDocs({
     app,
-    title: 'Youtube Sharing API Gateway document',
     endpoint: 'api-docs',
+    title: 'Youtube Sharing API Gateway document',
   });
 
   const port = ENV.PORT;
 
   await app.listen(port);
 
-  logger.verbose(`====== App listen on port ${port} ======`);
-  logger.verbose(`====== Swagger url: http://localhost:${port}/api-docs`);
+  logger.verbose(`====== App url: http://localhost:${port}/${globalPrefix}`);
+  logger.verbose(
+    `====== Swagger url: http://localhost:${port}/${swaggerEndpoint}`,
+  );
 }
+
 bootstrap();
